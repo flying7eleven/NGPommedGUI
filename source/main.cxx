9d24294758dd52cfe37fba9ca7249f1dc0e311e3
Tim Hütz <tim@huetz.biz>
2015-02-10 16:40:40 +0100

    Added code to fork a daemon process

--- source/main.cxx
+++ source/main.cxx
@@ -1,3 +1,22 @@
+#include <sys/types.h> // pid_t
+#include <stdlib.h> // exit
+#include <unistd.h>
+#include <sched.h> // fork, _exit
+
 int main( int argc, char **argv ) {
-	return 0;
+	// fork the process
+	pid_t processId = fork();
+
+	// if the fork returned -1 as a process id of the child, the fork failed
+	if( -1 == processId ) {
+		exit( EXIT_FAILURE + 0 );
+
+	}
+	// if the new process id is larger than 0, we are still in the parent process and can exit here
+	else if( processId > 0 ) {
+		exit( EXIT_SUCCESS );
+	}
+
+	// everything went okay
+	return EXIT_SUCCESS;
 }
